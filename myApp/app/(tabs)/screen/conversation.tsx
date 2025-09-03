477dfee7323faff1bb56791b78f1bada6cc6da39
Onebluesky882 <onebluesky882@outlook.com>
2025-09-03 15:53:24 +0700

    build record voice

--- myApp/app/(tabs)/screen/conversation.tsx
+++ myApp/app/(tabs)/screen/conversation.tsx
@@ -5,13 +5,15 @@ import {
   useAudioRecorder,
   useAudioRecorderState,
 } from "expo-audio";
-import { useEffect } from "react";
+import { Audio } from "expo-av";
+import { useEffect, useState } from "react";
 import { Alert, Button, StyleSheet, View } from "react-native";
 
 export default function App() {
   const audioRecorder = useAudioRecorder(RecordingPresets.HIGH_QUALITY);
   const recorderState = useAudioRecorderState(audioRecorder);
 
+  const [sound, setSound] = useState<Audio.Sound | null>(null);
   const record = async () => {
     await audioRecorder.prepareToRecordAsync();
     audioRecorder.record();
@@ -36,12 +38,36 @@ export default function App() {
     })();
   }, []);
 
+  const playRecording = async () => {
+    if (!audioRecorder.uri) {
+      Alert.alert("no recording available");
+      return;
+    }
+
+    if (sound) {
+      await sound.playAsync();
+      setSound(null);
+    }
+
+    const { sound: newSound } = await Audio.Sound.createAsync(
+      {
+        uri: audioRecorder.uri,
+      },
+      { shouldPlay: true }
+    );
+
+    setSound(newSound);
+  };
   return (
     <View style={styles.container}>
       <Button
         title={recorderState.isRecording ? "Stop Recording" : "Start Recording"}
         onPress={recorderState.isRecording ? stopRecording : record}
       />
+
+      <View>
+        <Button title="Play Recording" onPress={playRecording} />
+      </View>
     </View>
   );
 }
